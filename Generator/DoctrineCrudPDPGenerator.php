4afa84d611f8536b293e1a7911c9e1dd51956091
pingdecopong <pingdecopong@gmail.com>
2013-07-22 02:46:28 +0900

    edit.

--- Generator/DoctrineCrudPDPGenerator.php
+++ Generator/DoctrineCrudPDPGenerator.php
@@ -52,6 +52,7 @@ class DoctrineCrudPDPGenerator extends DoctrineCrudGenerator
             'entity_namespace'  => $entityNamespace,
             'format'            => $this->format,
             'fields'            => $this->metadata->fieldMappings,
+            'associations'      => $this->getAssociationMetadata($this->metadata),
         ));
     }
 
@@ -70,6 +71,33 @@ class DoctrineCrudPDPGenerator extends DoctrineCrudGenerator
         $this->generateLayoutView($dir);
     }
 
+    protected function generateIndexView($dir)
+    {
+        $this->renderFile('crud/views/index.html.twig.twig', $dir.'/index.html.twig', array(
+            'bundle'            => $this->bundle->getName(),
+            'entity'            => $this->entity,
+            'fields'            => $this->metadata->fieldMappings,
+            'actions'           => $this->actions,
+            'record_actions'    => $this->getRecordActions(),
+            'route_prefix'      => $this->routePrefix,
+            'route_name_prefix' => $this->routeNamePrefix,
+            'associations'      => $this->getAssociationMetadata($this->metadata),
+        ));
+    }
+
+    protected function generateShowView($dir)
+    {
+        $this->renderFile('crud/views/show.html.twig.twig', $dir.'/show.html.twig', array(
+            'bundle'            => $this->bundle->getName(),
+            'entity'            => $this->entity,
+            'fields'            => $this->metadata->fieldMappings,
+            'actions'           => $this->actions,
+            'route_prefix'      => $this->routePrefix,
+            'route_name_prefix' => $this->routeNamePrefix,
+            'associations'      => $this->getAssociationMetadata($this->metadata),
+        ));
+    }
+
     protected function generateDeleteView($dir)
     {
         $this->renderFile('crud/views/delete.html.twig.twig', $dir.'/delete.html.twig', array(
@@ -79,6 +107,7 @@ class DoctrineCrudPDPGenerator extends DoctrineCrudGenerator
             'actions'           => $this->actions,
             'route_prefix'      => $this->routePrefix,
             'route_name_prefix' => $this->routeNamePrefix,
+            'associations'      => $this->getAssociationMetadata($this->metadata),
         ));
     }
 
@@ -92,5 +121,44 @@ class DoctrineCrudPDPGenerator extends DoctrineCrudGenerator
             'route_prefix'      => $this->routePrefix,
             'route_name_prefix' => $this->routeNamePrefix,
         ));
+    }
+
+    protected function generateNewView($dir)
+    {
+        $this->renderFile('crud/views/new.html.twig.twig', $dir.'/new.html.twig', array(
+            'bundle'            => $this->bundle->getName(),
+            'entity'            => $this->entity,
+            'route_prefix'      => $this->routePrefix,
+            'route_name_prefix' => $this->routeNamePrefix,
+            'actions'           => $this->actions,
+            'fields'            => $this->metadata->fieldMappings,
+            'associations'      => $this->getAssociationMetadata($this->metadata),
+        ));
+    }
+
+    protected function generateEditView($dir)
+    {
+        $this->renderFile('crud/views/edit.html.twig.twig', $dir.'/edit.html.twig', array(
+            'route_prefix'      => $this->routePrefix,
+            'route_name_prefix' => $this->routeNamePrefix,
+            'entity'            => $this->entity,
+            'bundle'            => $this->bundle->getName(),
+            'actions'           => $this->actions,
+            'fields'            => $this->metadata->fieldMappings,
+            'associations'      => $this->getAssociationMetadata($this->metadata),
+        ));
+    }
+
+    private function getAssociationMetadata(ClassMetadataInfo $metadata)
+    {
+        $fields = array();
+
+        foreach ($metadata->associationMappings as $fieldName => $relation) {
+            if ($relation['type'] !== ClassMetadataInfo::ONE_TO_MANY) {
+                $fields[$fieldName] = $fieldName;
+            }
+        }
+
+        return $fields;
     }
 }
